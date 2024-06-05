7778b3b8ac0618c4e82671e8d9bf3940b96a1cba
Josh Marango <joshua.marango@ohme-ev.com>
2024-06-05 17:25:28 +0100

    removing initial stack

--- bin/ohme-devops-cdk-assessment.ts
+++ bin/ohme-devops-cdk-assessment.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { OhmeDevopsCdkAssessmentStack } from '../lib/ohme-devops-cdk-assessment-stack';
+import { DevAppStack } from '../lib/dev/devAppStack';
 
 const app = new cdk.App();
-new OhmeDevopsCdkAssessmentStack(app, 'OhmeDevopsCdkAssessmentStack', {
+new DevAppStack(app, 'DevAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
